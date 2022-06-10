e2c98d8c3384b6bae1c4cfe5d83189b343aaee9f
Christian <suky@tiroler-ac.at>
2022-06-10 14:23:08 +0200

    directory change

---
+++ dist/credentials/vCloudDirector.credentials.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.vCloudDirector = void 0;
+class vCloudDirector {
+    constructor() {
+        this.name = 'vCloudDirector';
+        this.displayName = 'VMware vCloud Director';
+        this.properties = [
+            {
+                displayName: 'Host',
+                name: 'host',
+                type: 'string',
+                default: 'https://',
+            },
+            {
+                displayName: 'Username',
+                name: 'username',
+                type: 'string',
+                default: '',
+            },
+            {
+                displayName: 'Password',
+                name: 'password',
+                type: 'string',
+                default: '',
+            },
+        ];
+    }
+}
+exports.vCloudDirector = vCloudDirector;
+//# sourceMappingURL=vCloudDirector.credentials.js.map
